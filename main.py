3a45e2f9e85a7d4ddc8fc3af88b81ce3516e4037
joshuahufford <joshuamh1135@gmail.com>
2017-10-19 09:33:31 -0400

    10/18/2017

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import kivy
+from kivy.clock import Clock
 kivy.require('1.10.0')
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.widget import Widget
+from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty, BooleanProperty)
 from kivy.uix.floatlayout import FloatLayout
 
-class MainScreen(Screen):
-	pass
 
-class GameScreen(Screen):
-	pass
+class ScrollerGame(Widget):
+	player = ObjectProperty(None)
 
-class SettingsScreen(Screen):
-	pass
+	def update(self, dt):
+		self.player.move(dt)
 
-class ScreenManagement(ScreenManager):
-	pass
+	def on_touch_down(self, touch):
+		if touch.x >= self.player.center_x:
+			self.player.velocity_x = 2
+			print("2")
 
+class Player(Widget):
+	def move(self, dt):
+		print("moved")
 
-mainkv = Builder.load_file("main.kv")
 
 class MainApp(App):
 	def build(self):
-		return mainkv
+		game = ScrollerGame()
+		Clock.schedule_interval(game.update(), 1.0/60.0)
+		return game
 
 if __name__ == '__main__':
-    MainApp().run()
+	MainApp().run()

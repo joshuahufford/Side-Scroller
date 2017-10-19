e510e576a67f3b5c2595397bbafb473de29c3feb
joshuahufford <joshuamh1135@gmail.com>
2017-10-19 10:21:39 -0400

    10/19/2017
    First with working movement.

--- main.py
+++ main.py
@@ -1,33 +1,55 @@
+import random
 import kivy
 from kivy.clock import Clock
 kivy.require('1.10.0')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty, BooleanProperty)
-from kivy.uix.floatlayout import FloatLayout
+from kivy.vector import Vector
 
 
 class ScrollerGame(Widget):
 	player = ObjectProperty(None)
+	obstical1 = ObjectProperty(None)
 
 	def update(self, dt):
 		self.player.move(dt)
+		self.obstical1.scroll(self.player.velocity_x)
 
 	def on_touch_down(self, touch):
 		if touch.x >= self.player.center_x:
 			self.player.velocity_x = 2
-			print("2")
+
+	def on_touch_up(self, touch):
+		if touch.x >= self.player.center_x:
+			self.player.velocity_x = 0
 
 class Player(Widget):
+	MAX_VELOCITY_X = 10
+	velocity_x = NumericProperty(0)
+	velocity = ReferenceListProperty(velocity_x)
+
 	def move(self, dt):
-		print("moved")
+		if self.velocity_x > 0:
+			self.velocity_x += 2 * dt
+		if self.velocity_x > self.MAX_VELOCITY_X:
+			self.velocity_x = self.MAX_VELOCITY_X
 
+class Obstical(Widget):
+	LEFT_OFFSET = 10
+	
+	def scroll(self, scroll_dist):
+		if self.pos[0] > 0 - self.LEFT_OFFSET:
+			self.pos = Vector(-1 * scroll_dist, 0) + self.pos
+		else:
+			self.pos = Vector(
+				self.parent.width + random.randint(0, self.parent.width), 15)
 
-class MainApp(App):
+class ScrollerApp(App):
 	def build(self):
 		game = ScrollerGame()
-		Clock.schedule_interval(game.update(), 1.0/60.0)
+		Clock.schedule_interval(game.update, 1.0/60.0)
 		return game
 
 if __name__ == '__main__':
-	MainApp().run()
+	ScrollerApp().run()
